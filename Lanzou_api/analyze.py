637881efb02bc11d1a6381b2f0dd303f0734b466
Jaiwei <38151187+Jai-wei@users.noreply.github.com>
2023-11-24 00:51:09 +0800

    up

---
+++ Lanzou_api/analyze.py
@@ -0,0 +1,117 @@
+import requests
+import re
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6'
+}
+
+# 取中间字符串
+def extract_text(input_text, left_condition, right_condition):
+    # 构建正则表达式
+    pattern = re.compile(
+        f'{re.escape(left_condition)}(.*?){re.escape(right_condition)}')
+
+    # 使用正则表达式查找匹配项
+    match = re.findall(pattern, input_text)
+    return match
+
+
+def urlanalyze(url_param, psw_param, fname_param):
+    result_downurl = {}
+    ###### 1 根据分享链接及密码并获取参数
+    try:
+        filelist = fname_param.split(',')
+        main_url = extract_text(url_param, '://', '.com')[0]
+        response = requests.get(url_param, headers=headers)
+        if response.status_code != 200:
+            return f"{url_param} 请求失败，状态码：{response.status_code}"
+        else:
+            pg1 = extract_text(response.text, "'pg':", ",")[0]
+            t1 = extract_text(response.text, "'t':", ",")[0]
+            k1 = extract_text(response.text, "'k':", ",")[0]
+            data = {
+                'lx': extract_text(response.text, "'lx':", ",")[0],
+                'fid': extract_text(response.text, "'fid':", ",")[0],
+                'uid': extract_text(response.text, "'uid':'", "'")[0],
+                'pg': extract_text(response.text, f"{pg1} =", ";")[0],
+                'rep': extract_text(response.text, "'rep':'", "'")[0],
+                't': extract_text(response.text, f"var {t1} = '", "'")[0],
+                'k': extract_text(response.text, f"var {k1} = '", "'")[0],
+                'up': extract_text(response.text, "'up':", ",")[0],
+                'ls': extract_text(response.text, "'ls':", ",")[0],
+                'pwd': psw_param
+            }
+    except:
+        return f"{url_param} 请求失败"
+    ###### 2 根据密码发送post请求
+    try:
+        response = requests.post("https://" + main_url + ".com/filemoreajax.php", headers=headers, data=data)
+        if response.status_code != 200:
+            return f"https://{main_url}.com/filemoreajax.php 请求失败，状态码：{response.status_code}"
+        data = response.json()
+        if data['info'] != 'sucess':
+            return f"https://{main_url}.com/filemoreajax.php 请求失败，状态码：{response.status_code}"
+    except:
+        return f"https://{main_url}.com/filemoreajax.php 请求失败"
+    ###### 3 解析返回的json
+    try:
+        for ii in data['text']:
+            if ii['name_all'] in filelist:
+    ###### 4 解析出文件id, 并获取fn
+                try:
+                    response = requests.get("https://" + main_url + ".com/"+ii['id'], headers=headers)
+                    if response.status_code != 200:
+                        return f"https://{main_url}.com/{ii['id']} 请求失败，状态码：{response.status_code}"
+                    else:
+                        fn = extract_text(response.text, 'src="', '" frameborder')[1]
+                except:
+                    return f"https://{main_url}.com/{ii['id']} 请求失败"
+    ###### 5 解析fn
+                try:
+                    response = requests.get("https://" + main_url + ".com/"+fn, headers=headers)
+                    if response.status_code != 200:
+                        return f"https://{main_url}.com/{fn} 请求失败，状态码：{response.status_code}"
+                    else:
+                        signs1 = extract_text(response.text, "'signs':", ",")[0]
+                        websignkey1 = extract_text(response.text, "'websignkey':", ",")[0]
+                        data = {
+                            'action': extract_text(response.text, "'action':'", "'")[0],
+                            'signs': extract_text(response.text, f"{signs1} = '", "'")[0],
+                            'sign': extract_text(response.text, "'sign':'", "'")[0],
+                            'websign': "",
+                            'websignkey': extract_text(response.text, f"{websignkey1} = '", "'")[0],
+                            'ves': extract_text(response.text, "'ves':", " ")[0]
+                        }
+                except:
+                    return f"https://{main_url}.com/{fn} 请求失败"
+    ###### 6 获取伪直链
+                try:
+                    headers2 = {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0',
+                        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6',
+                        'Referer': "https://" + main_url + ".com/"+fn
+                    }
+                    response = requests.post("https://"+main_url+".com/ajaxm.php", headers=headers2, data=data)
+                    if response.status_code != 200:
+                        return f"https://{main_url}.com/ajaxm.php 请求失败，状态码：{response.status_code}"
+                    else:
+                        data = response.json()
+                        fake_downurl = data['dom'] + '/file/' + data['url']
+                except:
+                    return f"https://{main_url}.com/ajaxm.php 请求失败"
+    ###### 7 获取真直链
+                try:
+                    response = requests.get(fake_downurl, headers=headers, allow_redirects=False)
+                    if response.status_code != 200:
+                        return f"{fake_downurl} 伪直链请求失败，状态码：{response.status_code}"
+                    else:
+                        for key, value in response.headers.items():
+                            if key == 'Location':
+                                result_downurl[ii['name_all']] = value
+                                # true_downurl = value
+                except:
+                    return f"{fake_downurl} 伪直链请求失败"
+        return f"{result_downurl}"
+    except:
+        pass
--- Lanzou_api/views.py
+++ Lanzou_api/views.py
@@ -1,22 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from Lanzou_api.analyze import urlanalyze
+
+
 
 def index(request):
     url_param = request.GET.get('url', '')
     psw_param = request.GET.get('psw', '')
+    fname_param = request.GET.get('fname', '')
     context = {
-        'url_param': url_param,
-        'psw_param': psw_param,
+        'download_url': urlanalyze(url_param, psw_param, fname_param)
     }
     # 渲染模板并返回HttpResponse
     return render(request, 'Lanzou_api.html', context)
-    # html = f'''
-    # <html>
-    #     <body>
-    #         <h1>Hello from Vercel!</h1>
-    #         <p>The current time is1.</p>
-    #     </body>
-    # </html>
-    # '''
-    # return HttpResponse(html)
-
