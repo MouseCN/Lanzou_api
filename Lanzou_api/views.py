07d40524c917b80a7dbecc29fb9e8d909c1e7540
Jaiwei <38151187+Jai-wei@users.noreply.github.com>
2023-11-23 22:25:26 +0800

    up

--- Lanzou_api/views.py
+++ Lanzou_api/views.py
@@ -2,11 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    url_param = request.GET.get('url', '')
-    psw_param = request.GET.get('psw', '')
-    context = {
-        'url_param': url_param,
-        'psw_param': psw_param,
-    }
-    # 渲染模板并返回HttpResponse
-    return render(request, 'Lanzou_api.html', context)
+    # url_param = request.GET.get('url', '')
+    # psw_param = request.GET.get('psw', '')
+    # context = {
+    #     'url_param': url_param,
+    #     'psw_param': psw_param,
+    # }
+    # # 渲染模板并返回HttpResponse
+    # return render(request, 'Lanzou_api.html', context)
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is1.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
