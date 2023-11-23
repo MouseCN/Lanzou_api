740b79653048a404fc585cc8b03718a1e1e9078b
Jaiwei <38151187+Jai-wei@users.noreply.github.com>
2023-11-23 23:33:47 +0800

    up

--- Lanzou_api/settings.py
+++ Lanzou_api/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,7 @@ SECRET_KEY = "django-insecure-n(neff@ure*zyr=vewru2lk%e60$7n3$mt6#$vo!kpw+g$*rj7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []#['127.0.0.1','Lanzou_api.application']
 
 
 # Application definition
