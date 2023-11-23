6506f3e053e3c2ae5e8efe3d3b0ac1f7b0fab6ab
Jaiwei <38151187+Jai-wei@users.noreply.github.com>
2023-11-23 23:41:50 +0800

    up

--- Lanzou_api/settings.py
+++ Lanzou_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-n(neff@ure*zyr=vewru2lk%e60$7n3$mt6#$vo!kpw+g$*rj7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','Lanzou_api.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
