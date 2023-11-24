b6063a1b75ec6730f15edd5100165de8004eefdf
Jaiwei <38151187+Jai-wei@users.noreply.github.com>
2023-11-24 13:36:39 +0800

    up

--- Lanzou_api/analyze.py
+++ Lanzou_api/analyze.py
@@ -16,8 +16,8 @@ def extract_text(input_text, left_condition, right_condition):
     match = re.findall(pattern, input_text)
     return match
 
-
-def urlanalyze(url_param, psw_param, fname_param):
+### 给定3个参数-分享的文件夹链接 带密码
+def param3(url_param, psw_param, fname_param):
     result = ''
     ###### 1 根据分享链接及密码并获取参数
     try:
@@ -116,3 +116,75 @@ def urlanalyze(url_param, psw_param, fname_param):
             return "请求失败faild-400"
     except:
         pass
+
+### 给定2个参数-分享的是文件链接 带密码
+def param2(url_param, psw_param):
+    pass
+
+### 给定1个参数-分享的是文件 不带密码
+def param1(url_param):
+    result = ''
+    ###### 1 根据文件id, 并获取fn
+    try:
+        main_url = extract_text(url_param, '://', '.com')[0]
+        response = requests.get(url_param, headers=headers)
+        if response.status_code != 200:
+            return f"{url_param} 请求失败faild-400，状态码：{response.status_code}"
+        else:
+            fn = extract_text(
+                response.text, 'src="', '" frameborder')[1]
+    except:
+        return f"{url_param} 请求失败faild-400"
+###### 2 解析fn
+    try:
+        response = requests.get(
+            "https://" + main_url + ".com/"+fn, headers=headers)
+        if response.status_code != 200:
+            return f"https://{main_url}.com/{fn} 请求失败faild-400，状态码：{response.status_code}"
+        else:
+            signs1 = extract_text(
+                response.text, "'signs':", ",")[0]
+            websignkey1 = extract_text(
+                response.text, "'websignkey':", ",")[0]
+            data = {
+                'action': extract_text(response.text, "'action':'", "'")[0],
+                'signs': extract_text(response.text, f"{signs1} = '", "'")[0],
+                'sign': extract_text(response.text, "'sign':'", "'")[0],
+                'websign': "",
+                'websignkey': extract_text(response.text, f"{websignkey1} = '", "'")[0],
+                'ves': extract_text(response.text, "'ves':", " ")[0]
+            }
+    except:
+        return f"https://{main_url}.com/{fn} 请求失败faild-400"
+###### 3 获取伪直链
+    try:
+        headers2 = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6',
+            'Referer': "https://" + main_url + ".com/"+fn
+        }
+        response = requests.post(
+            "https://"+main_url+".com/ajaxm.php", headers=headers2, data=data)
+        if response.status_code != 200:
+            return f"https://{main_url}.com/ajaxm.php 请求失败faild-400，状态码：{response.status_code}"
+        else:
+            data = response.json()
+            fake_downurl = data['dom'] + '/file/' + data['url']
+    except:
+        return f"https://{main_url}.com/ajaxm.php 请求失败faild-400"
+###### 4 获取真直链
+    try:
+        response = requests.get(
+            fake_downurl, headers=headers, allow_redirects=False)
+        if response.status_code != 302:
+            return f"{fake_downurl} 伪直链请求失败faild-400，状态码：{response.status_code}"
+        else:
+            for key, value in response.headers.items():
+                if key == 'Location':
+                    result = value
+                    return value
+                    # true_downurl = value
+    except:
+        return f"{fake_downurl} 伪直链请求失败faild-400"
+    if result == '':
+        return "请求失败faild-400"
--- Lanzou_api/views.py
+++ Lanzou_api/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from Lanzou_api.analyze import urlanalyze
+from Lanzou_api.analyze import *
 
 
 
@@ -8,8 +8,17 @@ def index(request):
     url_param = request.GET.get('url', '')
     psw_param = request.GET.get('psw', '')
     fname_param = request.GET.get('fname', '')
-    context = {
-        'download_url': urlanalyze(url_param, psw_param, fname_param)
-    }
+    if url_param != '' and psw_param != '' and fname_param != '':
+        context = {
+            'download_url': param3(url_param, psw_param, fname_param)
+        }
+    elif url_param != '':
+        context = {
+            'download_url': param1(url_param)
+        }
+    else:
+        context = {
+            'download_url': '请正确传入get参数  =>>  /?url=? & psw=? & fname=?'
+        }
     # 渲染模板并返回HttpResponse
     return render(request, 'Lanzou_api.html', context)
